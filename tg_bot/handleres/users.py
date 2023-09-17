88e70efaac6e0f3f004fd757c13de9ff597c3e4f
KyrylRyndych <53616293+KyrylRyndych@users.noreply.github.com>
2023-09-17 20:52:55 +0300

    fix class location

--- tg_bot/handleres/users.py
+++ tg_bot/handleres/users.py
@@ -6,25 +6,25 @@ from tg_bot.misc.texts import START_TEXT, LOCATION_TEXT, OUR_CONTACTS_TEXT
 from tg_bot.misc.locations import location
 
 
-async def start_handler(message: types.message):
+async def start_handler(message: types.message):  #handler start command
     await message.answer(START_TEXT, parse_mode="html", reply_markup=create_start_kb())
 
 
-async def where_are_we_handler(message: types.message, bot: Bot):
+async def where_are_we_handler(message: types.message, bot: Bot): # handler for "Where we are"
     await message.answer(LOCATION_TEXT)
     await bot.send_location(chat_id=message.from_user.id, latitude=location['truskav'].latitude, longitude=location['truskav'].longitude)
     await bot.send_location(chat_id=message.from_user.id, latitude=location['zelena'].latitude, longitude=location['zelena'].longitude)
 
 
-async def our_contacts_handler(message: types.message):
-        await message.answer(OUR_CONTACTS_TEXT)
+async def our_contacts_handler(message: types.message): # about us handler
+    await message.answer(OUR_CONTACTS_TEXT)
 
 
-async def in_development_handler(message: types.message):
+async def in_development_handler(message: types.message): # pass handler 
     await message.answer("Ця опція в розробці")
 
 
-def register_start(dp: Dispatcher):
+def register_start(dp: Dispatcher):   #register all handlers
     dp.register_message_handler(start_handler, commands=['start'])
     dp.register_message_handler(where_are_we_handler, Text("Де знаходяться магазини"))
     dp.register_message_handler(our_contacts_handler, Text("Наші контакти"))
--- tg_bot/misc/locations.py
+++ tg_bot/misc/locations.py
@@ -1,17 +1,11 @@
 class Point():
-    def __init__(self) -> None:
-        self.latitude = None
-        self.longitude = None
+    def __init__(self, lat, long) -> None:
+        self.latitude = lat
+        self.longitude = long
 
 
-truskav = Point()
-truskav.latitude = 49.797625
-truskav.longitude = 24.011280
-
-zelena  = Point()
-zelena.latitude = 49.8120419374225
-zelena.longitude = 24.061463338369506
-
+truskav = Point(49.797625, 24.011280 )
+zelena  = Point(49.8120419374225, 24.061463338369506)
 
 location = {"truskav" : truskav,
             "zelena": zelena}
